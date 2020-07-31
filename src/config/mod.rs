6342a16393edc36d4b71b0e01f1cdbac6b61b7b6
ioniDev <ioni_dev@protonmail.com>
2020-07-31 18:42:37 -0300

    new handles and db integration

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,14 +1,24 @@
+pub mod crypto;
+
 use color_eyre::Result;
 use eyre::WrapErr;
-use serde::Deserialize;
 use dotenv::dotenv;
+
+use serde::Deserialize;
+use sqlx::postgres::PgPool;
+
+use crypto::CryptoService;
+use std::sync::Arc;
 use tracing::{info, instrument};
 use tracing_subscriber::EnvFilter;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub host: String,
-    pub port: u16
+    pub port: u16,
+    pub database_url: String,
+    pub secret_key: String,
+    pub jwt_secret: String
 }
 
 impl Config {
@@ -24,9 +34,28 @@ impl Config {
         info!("Loading Config");
         // using config crate to get the variables from env to the config struct
         let mut c = config::Config::new();
+
         c.merge(config::Environment::default())?;
 
         c.try_into()
             .context("Loading variables from env")
+    }
+
+    #[instrument(skip(self))]
+    pub async fn db_pool(&self) -> Result<PgPool> {
+        info!("Creating database connection pool.");
+        PgPool::builder()
+            .connect_timeout(std::time::Duration::from_secs(30))
+            .build(&self.database_url)
+            .await
+            .context("creating database connection pool")
+    }
+
+    #[instrument(skip(self))]
+    pub fn hashing(&self) -> CryptoService {
+        CryptoService {
+            key: Arc::new(self.secret_key.clone()),
+            jwt_secret: Arc::new(self.jwt_secret.clone()),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
+#[macro_use]
+extern crate validator_derive;
+
 mod config;
+mod db;
+mod errors;
 mod handlers;
+mod models;
 
 use color_eyre::Result;
 use crate::config::Config;
-use actix_web::{App, HttpServer, middleware::Logger};
-use tracing::info;
+use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer};
+use tracing::{info, instrument};
 use handlers::app_config;
 
 #[actix_rt::main]
+#[instrument]
 async fn main() -> Result<()> {
 
     let config = Config::from_env().expect("Server configuration");
+
+    let pool = config.db_pool().await.expect("Database configuration");
+
+    let hashing = config.hashing();
 
     info!("Starting server at http://{}:{}/", config.host, config.port);
     // instantiation of server
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
+            .data(pool.clone())
+            .data(hashing.clone())
             .configure(app_config)
     })
     .bind(format!("{}:{}", config.host, config.port))?
